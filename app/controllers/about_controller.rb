80ec640753386cae71d12e7d7c7fa5f88acc3f9e
R Verhey - FedScoop <ryanverhey@RWVMac-2.local>
2014-09-26 15:01:08 -0400

    Failed attempt to load variables for all about views

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,5 +1,11 @@
 class AboutController < ApplicationController
 
+  # def initialize
+  #   @next_event = Event.where('date > ?', DateTime.now).order("date").first
+  #   events = Event.where('date > ?', DateTime.now).order("date")
+  #   @events_this_year = events.inject([]) { |arr, event| arr << event if event.date.year == DateTime.now.year }
+  # end
+
   def index
     @speakers = Speaker.all.limit 8
     @next_event = Event.where('date > ?', DateTime.now).order("date").first
