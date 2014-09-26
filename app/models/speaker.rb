779a4e8ae3332e131a4612ff426c7a98dea82e37
R Verhey - FedScoop <ryanverhey@RWVMac-2.local>
2014-09-26 15:03:53 -0400

    Added method for returning speaker name

--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -2,4 +2,8 @@ class Speaker < ActiveRecord::Base
   has_many :speaking_engagements
   has_many :events, through: :speaking_engagements
 
+  def name
+    return "#{self.first_name} #{self.last_name}"
+  end
+
 end
