5b801cfa0cf1d29089075b254f17e44752be206d
R Verhey - FedScoop <ryanverhey@RWVMac-2.local>
2014-09-26 14:57:49 -0400

    Speakers method for speakers page

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -7,4 +7,11 @@ class AboutController < ApplicationController
     @events_this_year = events.inject([]) { |arr, event| arr << event if event.date.year == DateTime.now.year }
     @about = About.first.homepage_text
   end
+
+  def speakers
+    @speakers = Speaker.all
+    @next_event = Event.where('date > ?', DateTime.now).order("date").first
+    events = Event.where('date > ?', DateTime.now).order("date")
+    @events_this_year = events.inject([]) { |arr, event| arr << event if event.date.year == DateTime.now.year }
+  end
 end
