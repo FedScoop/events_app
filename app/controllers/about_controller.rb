279d4eee3eae992993bd178f6931cfa0a6c768f1
R Verhey - FedScoop <ryanverhey@RWVMac-2.local>
2014-09-26 14:57:10 -0400

    Index method for homepage

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,3 +1,10 @@
 class AboutController < ApplicationController
 
+  def index
+    @speakers = Speaker.all.limit 8
+    @next_event = Event.where('date > ?', DateTime.now).order("date").first
+    events = Event.where('date > ?', DateTime.now).order("date")
+    @events_this_year = events.inject([]) { |arr, event| arr << event if event.date.year == DateTime.now.year }
+    @about = About.first.homepage_text
+  end
 end
