a922e5f472b1f11cec534797d1b8003f9f3119bb
R Verhey - FedScoop <ryanverhey@RWVMac-2.local>
2014-09-26 14:59:14 -0400

    About method for about page

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -14,4 +14,10 @@ class AboutController < ApplicationController
     events = Event.where('date > ?', DateTime.now).order("date")
     @events_this_year = events.inject([]) { |arr, event| arr << event if event.date.year == DateTime.now.year }
   end
+
+  def about
+    @next_event = Event.where('date > ?', DateTime.now).order("date").first
+    events = Event.where('date > ?', DateTime.now).order("date")
+    @events_this_year = events.inject([]) { |arr, event| arr << event if event.date.year == DateTime.now.year }
+  end
 end
