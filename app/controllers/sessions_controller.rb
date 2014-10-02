c692ced1fd27fafc39a8155f9f3f51413d41f55e
R Verhey - FedScoop <ryanverhey@RWVMac-2.local>
2014-10-02 17:39:34 -0400

    Added Sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,32 @@
+class SessionsController < ApplicationController
+  def create
+    @user = User.find_by_email(user_params[:email])
+    if @user && @user.authenticate(user_params[:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      flash[:error] = "Your email or password was incorect"
+      redirect_to login_path
+    end
+  end
+
+  def new
+    if logged_in?
+      redirect_to user_path(current_user)
+    else
+      @user = User.new
+    end
+  end
+
+  def logout
+    session.clear
+    flash[:logout] = "You have been logged out"
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
