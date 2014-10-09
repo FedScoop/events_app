a56a4f0be870886be0fe654881c113afc36db78c
R Verhey - FedScoop <ryanverhey@RWVMac-2.local>
2014-10-09 13:51:40 -0400

    Added resources for speakers

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,7 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
 
-  # ABOUT
+  # ABOUT -----------------------------------------------------------------
   root 'about#index'
   get 'about/speakers' => 'about#speakers'
   get 'about' => 'about#about'
@@ -62,12 +62,15 @@ Rails.application.routes.draw do
   get 'about/sponsor' => 'about#sponsor'
   get 'about/request-to-speak' => 'about#request_to_speak'
 
-  # USERS
+  # USERS -----------------------------------------------------------------
   resources :users, only: [:new, :create, :show]
   get 'users/validate/:id' => 'users#validate'
 
-  # SESSIONS
+  # SESSIONS --------------------------------------------------------------
   get 'login' => 'sessions#new'
   post 'sessions' => 'sessions#create'
   get 'logout' => 'sessions#logout'
+
+  # SPEAKERS --------------------------------------------------------------
+  resources :speakers
 end
