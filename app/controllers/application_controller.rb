7ee2778ab9a6d6faa2c20234214e61e7d9388d7e
R Verhey - FedScoop <ryanverhey@RWVMac-2.local>
2014-10-10 11:40:51 -0400

    Made logged_in? method check to see if the user found is even in the database (in case someone needs to be deleted)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   helper_method :logged_in?, :current_user
 
   def logged_in?
-    !!session[:user_id]
+    !!User.find_by_id(session[:user_id])
   end
 
   def current_user
