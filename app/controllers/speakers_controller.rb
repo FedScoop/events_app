acc93dfcee76199b3bc229de8be8be363d7df0d4
R Verhey - FedScoop <ryanverhey@RWVMac-2.local>
2014-10-10 15:16:55 -0400

    Added photo_url to speaker_params

--- app/controllers/speakers_controller.rb
+++ app/controllers/speakers_controller.rb
@@ -34,7 +34,7 @@ class SpeakersController < ApplicationController
   private
 
   def speaker_params
-    params.require(:speaker).permit(:first_name, :last_name, :job_title, :employer, :bio)
+    params.require(:speaker).permit(:first_name, :last_name, :job_title, :employer, :bio, :photo_url)
   end
 
 end
