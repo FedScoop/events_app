5af74c93c9b72ab9041c5c7ad2bd76bec6ec9310
R Verhey - FedScoop <ryanverhey@RWVMac-2.local>
2014-10-10 11:50:29 -0400

    Added update and speaker_params methods in speaker controller

--- app/controllers/speakers_controller.rb
+++ app/controllers/speakers_controller.rb
@@ -12,4 +12,17 @@ class SpeakersController < ApplicationController
     logged_in? ? @speaker = Speaker.find_by_id(params[:id]) : not_logged_in
   end
 
+  def update
+    s_params = speaker_params
+    s_params[:employer] = Agency.find_by_id s_params[:employer]
+    Speaker.update(params[:id], s_params)
+    redirect_to speaker_path(Speaker.find_by_id params[:id])
+  end
+
+  private
+
+  def speaker_params
+    params.require(:speaker).permit(:first_name, :last_name, :job_title, :employer, :bio)
+  end
+
 end
