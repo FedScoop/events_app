85fabfddf313613b84be91a842f412c023c3118e
R Verhey - FedScoop <ryanverhey@RWVMac-2.local>
2014-10-10 11:47:43 -0400

    Added show route to speaker controller

--- app/controllers/speakers_controller.rb
+++ app/controllers/speakers_controller.rb
@@ -4,4 +4,8 @@ class SpeakersController < ApplicationController
     logged_in? ? @speakers = Speaker.all.order("last_name") : not_logged_in
   end
 
+  def show
+    logged_in? ? @speaker = Speaker.find_by_id(params[:id]) : not_logged_in
+  end
+
 end
