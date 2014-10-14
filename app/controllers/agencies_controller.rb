7730a458fa3797781252b023a25c4882b8b5f3fb
Ryan Verhey <ryan90v@gmail.com>
2014-10-14 17:44:44 -0400

    Added agencies#show method

--- app/controllers/agencies_controller.rb
+++ app/controllers/agencies_controller.rb
@@ -4,4 +4,16 @@ class AgenciesController < ApplicationController
     if_logged_in { @agencies = Agency.all.order "name" }
   end
 
+  def show
+    agency = Agency.find_by_id params[:id]
+    if logged_in? && agency
+      @agency = agency
+    elsif !agency
+      flash[:message] = "Sorry, that agency / employer doesn't exist!"
+      redirect_to agencies_path
+    else
+      not_logged_in
+    end
+  end
+
 end
