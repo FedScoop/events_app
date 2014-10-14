dc32c59f968a4eacaa13d3d6d8d051b5ae4ec549
R Verhey - FedScoop <ryanverhey@RWVMac-2.local>
2014-10-14 10:40:00 -0400

    Redirect to dashboard when login, switch error to message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,9 @@ class SessionsController < ApplicationController
     @user = User.find_by_email(user_params[:email])
     if @user && @user.authenticate(user_params[:password])
       session[:user_id] = @user.id
-      redirect_to root_path
-    else
-      flash[:error] = "Your email or password was incorect"
+      redirect_to dashboard_path
+    elsif @user
+      flash[:message] = "Your email or password was incorect"
       redirect_to login_path
     end
   end
