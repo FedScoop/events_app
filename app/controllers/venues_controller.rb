85bfbbf5217df67764a5bd72d96db81f5a604b0a
R Verhey - FedScoop <ryanverhey@RWVMac-2.local>
2014-10-14 11:16:36 -0400

    Added venues#show method

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -4,4 +4,16 @@ class VenuesController < ApplicationController
     if_logged_in { @venues = Venue.all.order("name") }
   end
 
+  def show
+    venue = Venue.find_by_id params[:id]
+    if logged_in? && venue
+      @venue = venue
+    elsif !venue
+      flash[:message] = "Sorry, that venue doesn't exist!"
+      redirect_to venues_path
+    else
+      not_logged_in
+    end
+  end
+
 end
