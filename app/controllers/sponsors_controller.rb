c51bf9498b13ab64427e0eb231945eea05b5b14a
R Verhey - FedScoop <ryanverhey@RWVMac-2.local>
2014-10-14 16:29:01 -0400

    Added sponsors#shoe method

--- app/controllers/sponsors_controller.rb
+++ app/controllers/sponsors_controller.rb
@@ -4,4 +4,8 @@ class SponsorsController < ApplicationController
     if_logged_in { @sponsors = Sponsor.all.order "name" }
   end
 
+  def show
+    if_logged_in { @sponsor = Sponsor.find_by_id params[:id] }
+  end
+
 end
