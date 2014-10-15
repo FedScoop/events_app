0de1287f9e5667b0040c39be168f610d31560d34
Ryan Verhey <ryan90v@gmail.com>
2014-10-15 14:31:04 -0400

    Added required delayed_job stuff in speakers model

--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -6,6 +6,8 @@ class Speaker < ActiveRecord::Base
   has_attached_file :photo, :styles => { :huge => "1000x1000#", :large => "700x700#", :medium => "300x300#", :thumb => "100x100#" }, :default_url => "/images/missing/missing_:style.jpg"
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 
+  process_in_background :photo
+
   def name
     return "#{self.first_name} #{self.last_name}"
   end
