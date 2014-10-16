fd3bfd07afa542fed5810b9bf7983d8f1f879aab
Ryan Verhey <ryan90v@gmail.com>
2014-10-16 11:11:56 -0400

    Added necessary delayed_job stuff in sponsors model

--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -4,4 +4,7 @@ class Sponsor < ActiveRecord::Base
 
   has_attached_file :photo, :styles => { :huge => "1000x1000#", :large => "700x700#", :medium => "300x300#", :thumb => "100x100#" }, :default_url => "/images/missing/missing_:style.jpg"
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
+
+  process_in_background :photo
+
 end
