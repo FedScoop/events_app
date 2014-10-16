e58003001864992116baf8896c27cdab2cbd7a1e
Ryan Verhey <ryan90v@gmail.com>
2014-10-16 11:10:16 -0400

    Schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141015161145) do
+ActiveRecord::Schema.define(version: 20141016143447) do
 
   create_table "abouts", force: true do |t|
     t.string "homepage_text"
@@ -73,9 +73,12 @@ ActiveRecord::Schema.define(version: 20141015161145) do
 
   create_table "sponsors", force: true do |t|
     t.string   "name"
-    t.string   "photo_url"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   create_table "sponsorships", force: true do |t|
