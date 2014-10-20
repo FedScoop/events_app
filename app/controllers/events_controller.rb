42992f056af2333a7841ed512409ab0862359ae6
Ryan Verhey <ryan90v@gmail.com>
2014-10-20 11:47:50 -0400

    Added events#show method

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,4 +4,8 @@ class EventsController < ApplicationController
     if_logged_in { @events_by_year = Event.events_by_year }
   end
 
+  def show
+    if_logged_in { @event = Event.find_by_id params[:id] }
+  end
+
 end
