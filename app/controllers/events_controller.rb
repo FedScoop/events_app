e86a6d8d44d9e12079075ff6f319496224799689
Ryan Verhey <ryan90v@gmail.com>
2014-10-28 17:05:15 -0400

    Started events#update, still need to complete

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -12,4 +12,16 @@ class EventsController < ApplicationController
     if_logged_in { @event = Event.find_by_id params[:id] }
   end
 
+  def update
+    event = params[:event]
+    puts event[:date]
+    puts DateTime.new(event["date(1i)"].to_i,
+                      event["date(2i)"].to_i,
+                      event["date(3i)"].to_i,
+                      event["date(4i)"].to_i,
+                      event["date(5i)"].to_i,
+                      0,
+                      ((Time.zone.utc_offset / 60) / 60).to_s)
+  end
+
 end
