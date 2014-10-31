357636c8480c5737520d13a33aa7c6f63e128eae
Ryan Verhey <ryan90v@gmail.com>
2014-10-31 10:25:33 -0400

    Downcases the email to save to DB

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   end
 
   def create
+    user_params[:email] = user_params[:email].downcase
     @user = User.new(user_params)
     @user.validated = false
     @user.admin = false
